f1136287e6628dde8f9a63933d54195cefd4bb99
RyzzTech <102559807+Ryzz123@users.noreply.github.com>
2022-12-12 19:47:37 +0700

    first

--- src/pages/AnimePagesById.jsx
+++ src/pages/AnimePagesById.jsx
@@ -1,21 +1,31 @@
-import { Box, Container, SimpleGrid } from '@chakra-ui/react';
+import { Box, Container, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CardAnimeById from '../components/CardAnimeById';
 import CardCoverHome from '../components/CardCoverHome';
+import './../components/Loader.css';
 import TableAnimeById from '../components/TableAnimeById';
 
 const AnimePagesById = () => {
   const [anime, setAnime] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const anime_params = useParams();
 
+  const bacKgroundLoad = useColorModeValue('blue.500','white')
+
   useEffect(() => {
+    window.scrollTo(0, 0); // fungsi untuk scroll ke atas halaman
     const getAnimeById = async () => {
       try {
-        const response = await axios.get(`https://api.jikan.moe/v4/anime/${anime_params.anime_id}`);
+        const response = await axios.get(
+          `https://api.jikan.moe/v4/anime/${anime_params.anime_id}`
+        );
         setAnime(response.data.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 800)
       } catch (err) {
         console.log(err);
       }
@@ -25,15 +35,25 @@ const AnimePagesById = () => {
 
   return (
     <Container mb={20} pt={[20, 20, 20]} maxW="container.xl">
-      <SimpleGrid columns={[1, 2, 2]} spacingX="20px" spacingY="20px">
-        <Box>
-          <CardAnimeById anime={anime} />
+      {loading ? (
+        <Box display="flex" justifyContent="center">
+          <Box
+            color={bacKgroundLoad}
+            background={bacKgroundLoad}
+            className="dots"
+          ></Box>
         </Box>
-        <Box>
-          <TableAnimeById anime={anime} />
-          <CardCoverHome />
-        </Box>
-      </SimpleGrid>
+      ) : (
+        <SimpleGrid columns={[1, 2, 2]} spacingX="20px" spacingY="20px">
+          <Box>
+            <CardAnimeById anime={anime} />
+          </Box>
+          <Box>
+            <TableAnimeById anime={anime} />
+            <CardCoverHome />
+          </Box>
+        </SimpleGrid>
+      )}
     </Container>
   );
 };
--- src/pages/MangaPageById.jsx
+++ src/pages/MangaPageById.jsx
@@ -1,42 +1,65 @@
-import { Box, Container, SimpleGrid } from "@chakra-ui/react";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import CardCoverHome from "../components/CardCoverHome";
-import CardMangaById from "../components/CardMangaById";
-import TableMangaById from "../components/TableMangaById";
+import {
+  Box,
+  Container,
+  SimpleGrid,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import CardCoverHome from '../components/CardCoverHome';
+import CardMangaById from '../components/CardMangaById';
+import './../components/Loader.css';
+import TableMangaById from '../components/TableMangaById';
 
 const MangaPageById = () => {
-    const [manga, setManga] = useState({});
+  const [manga, setManga] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    const manga_params = useParams();
+  const manga_params = useParams();
+  const bacKgroundLoad = useColorModeValue('blue.500', 'white');
 
-    useEffect(() => {
-        const getMangaById = async () => {
-            try {
-                const response = await axios.get(`https://api.jikan.moe/v4/manga/${manga_params.manga_id}`);
-                setManga(response.data.data)
-            } catch(err) {
-                console.log(err);
-            }
-        }
-        getMangaById();
-    }, [manga_params.manga_id])
-
+  useEffect(() => {
+    window.scrollTo(0, 0); // fungsi untuk scroll ke atas halaman
+    const getMangaById = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.jikan.moe/v4/manga/${manga_params.manga_id}`
+        );
+        setManga(response.data.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 800);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMangaById();
+  }, [manga_params.manga_id]);
 
   return (
     <Container mb={20} pt={[20, 20, 20]} maxW="container.xl">
-      <SimpleGrid columns={[1, 2, 2]} spacingX="20px" spacingY="20px">
-            <Box>
-                <CardMangaById manga={manga} />
-            </Box>
-            <Box>
-                <TableMangaById manga={manga} />
-                <CardCoverHome />
-            </Box>
+      {loading ? (
+        <Box display="flex" justifyContent="center">
+          <Box
+            color={bacKgroundLoad}
+            background={bacKgroundLoad}
+            className="dots"
+          ></Box>
+        </Box>
+      ) : (
+        <SimpleGrid columns={[1, 2, 2]} spacingX="20px" spacingY="20px">
+          <Box>
+            <CardMangaById manga={manga} />
+          </Box>
+          <Box>
+            <TableMangaById manga={manga} />
+            <CardCoverHome />
+          </Box>
         </SimpleGrid>
+      )}
     </Container>
-  )
+  );
 };
 
 export default MangaPageById;
